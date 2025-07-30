b5dadec490f316121c6d3547ba2bd91e951fd3fe
KevinShemili <kevinshemili5@gmail.com>
2025-07-30 16:07:16 +0200

    Refactor test

--- com.shemilikevin.app.tracker/src/test/java/com/shemilikevin/app/repository/mongo/ProjectMongoRepositoryTest.java
+++ com.shemilikevin.app.tracker/src/test/java/com/shemilikevin/app/repository/mongo/ProjectMongoRepositoryTest.java
@@ -6,7 +6,6 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.bson.codecs.configuration.CodecRegistry;
@@ -28,14 +27,22 @@ import de.bwaldvogel.mongo.MongoServer;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
 public class ProjectMongoRepositoryTest {
+
+	private static final String DATABASE_NAME = "db";
+	private static final String COLLECTION_NAME = "collection";
+	private static final String ID_1 = "1";
+	private static final String ID_2 = "2";
+	private static final String NAME_1 = "Desktop Application";
+	private static final String NAME_2 = "Web Application";
+	private static final String DESCRIPTION_1 = "Desktop Application Description";
+	private static final String DESCRIPTION_2 = "Web Application Description";
+
 	private static MongoServer mongoServer;
 	private static InetSocketAddress inetSocketAddress;
 
 	private MongoClient mongoClient;
 	private ProjectMongoRepository projectRepository;
 	private MongoCollection<Project> projectCollection;
-	private String databaseName = "db";
-	private String collectionName = "collection";
 
 	@BeforeClass
 	public static void setUpInMemoryServer() {
@@ -56,10 +63,10 @@ public class ProjectMongoRepositoryTest {
 		MongoClientOptions mongoClientOptions = MongoClientOptions.builder().codecRegistry(codecRegistry).build();
 
 		mongoClient = new MongoClient(new ServerAddress(inetSocketAddress), mongoClientOptions);
-		projectRepository = new ProjectMongoRepository(mongoClient, databaseName, collectionName);
-		MongoDatabase database = mongoClient.getDatabase(databaseName);
+		projectRepository = new ProjectMongoRepository(mongoClient, DATABASE_NAME, COLLECTION_NAME);
+		MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
 		database.drop();
-		projectCollection = database.getCollection(collectionName, Project.class);
+		projectCollection = database.getCollection(COLLECTION_NAME, Project.class);
 	}
 
 	@After
@@ -81,24 +88,23 @@ public class ProjectMongoRepositoryTest {
 	public void testGetAll_DatabaseHasProjects_ReturnsProjects() {
 
 		// Arrange
-		Project project1 = new Project("1", "Desktop Application", "Desktop Application");
-		Project project2 = new Project("2", "Web Application", "Web Application");
-		projectCollection.insertMany(Arrays.asList(project1, project2));
+		addProjectToDb(new Project(ID_1, NAME_1, DESCRIPTION_1));
+		addProjectToDb(new Project(ID_2, NAME_2, DESCRIPTION_2));
 
 		// Act
 		List<Project> projectList = projectRepository.getAll();
 
 		// Assert
 		assertThat(projectList).hasSize(2);
-		assertThat(projectList).containsExactly(new Project("1", "Desktop Application", "Desktop Application"),
-				new Project("2", "Web Application", "Web Application"));
+		assertThat(projectList).containsExactly(new Project(ID_1, NAME_1, DESCRIPTION_1),
+				new Project(ID_2, NAME_2, DESCRIPTION_2));
 	}
 
 	@Test
 	public void testFindById_NoMatchingIdInDatabase_ReturnsNull() {
 
 		// Act
-		Project project = projectRepository.findById("1");
+		Project project = projectRepository.findById(ID_1);
 
 		// Assert
 		assertThat(project).isNull();
@@ -108,61 +114,59 @@ public class ProjectMongoRepositoryTest {
 	public void testFindById_OnlyOneDatabaseEntry_EntryHasMatchingId_ReturnsProject() {
 
 		// Arrange
-		Project project1 = new Project("1", "Desktop Application", "Desktop Application");
-		projectCollection.insertOne(project1);
+		addProjectToDb(new Project(ID_1, NAME_1, DESCRIPTION_1));
 
 		// Act
-		Project project = projectRepository.findById("1");
+		Project project = projectRepository.findById(ID_1);
 
 		// Assert
 		assertThat(project).isNotNull();
-		assertThat(project).isEqualTo(new Project("1", "Desktop Application", "Desktop Application"));
+		assertThat(project).isEqualTo(new Project(ID_1, NAME_1, DESCRIPTION_1));
 	}
 
 	@Test
 	public void testFindById_ManyDatabaseEntries_OneOfThemHasMatchingId_ReturnsProject() {
 
 		// Arrange
-		Project project1 = new Project("1", "Desktop Application", "Desktop Application");
-		Project project2 = new Project("2", "Web Application", "Web Application");
-		projectCollection.insertMany(Arrays.asList(project1, project2));
+		addProjectToDb(new Project(ID_1, NAME_1, DESCRIPTION_1));
+		addProjectToDb(new Project(ID_2, NAME_2, DESCRIPTION_2));
 
 		// Act
-		Project project = projectRepository.findById("2");
+		Project project = projectRepository.findById(ID_2);
 
 		// Assert
 		assertThat(project).isNotNull();
-		assertThat(project).isEqualTo(new Project("2", "Web Application", "Web Application"));
+		assertThat(project).isEqualTo(new Project(ID_2, NAME_2, DESCRIPTION_2));
 	}
 
 	@Test
 	public void testSave_SavesProjectInTheDatabase() {
 
-		// Arrange
-		Project project1 = new Project("1", "Desktop Application", "Desktop Application");
-		ArrayList<Project> databaseProjects = new ArrayList<Project>();
-
 		// Act
-		projectRepository.save(project1);
+		projectRepository.save(new Project(ID_1, NAME_1, DESCRIPTION_1));
 
 		// Assert
-		projectCollection.find().into(databaseProjects);
-		assertThat(databaseProjects).containsExactly(project1);
+		assertThat(queryAllProjectsFromDb()).containsExactly(new Project(ID_1, NAME_1, DESCRIPTION_1));
 	}
 
 	@Test
 	public void testDelete_DeletesProjectFromTheDatabase() {
 
 		// Arrange
-		Project project1 = new Project("1", "Desktop Application", "Desktop Application");
-		projectCollection.insertOne(project1);
-		ArrayList<Project> databaseProjects = new ArrayList<Project>();
+		addProjectToDb(new Project(ID_1, NAME_1, DESCRIPTION_1));
 
 		// Act
-		projectRepository.delete("1");
+		projectRepository.delete(ID_1);
 
 		// Assert
-		projectCollection.find().into(databaseProjects);
-		assertThat(databaseProjects).isEmpty();
+		assertThat(queryAllProjectsFromDb()).isEmpty();
+	}
+
+	private void addProjectToDb(Project project) {
+		projectCollection.insertOne(project);
+	}
+
+	private List<Project> queryAllProjectsFromDb() {
+		return projectCollection.find().into(new ArrayList<Project>());
 	}
 }
