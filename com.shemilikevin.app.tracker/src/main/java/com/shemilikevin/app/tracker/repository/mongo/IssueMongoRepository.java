9edb235920f80f2a8c8be3451638718aa5fa84f4
KevinShemili <kevinshemili5@gmail.com>
2025-07-31 15:49:47 +0200

    Implement findAll to make test compile

--- com.shemilikevin.app.tracker/src/main/java/com/shemilikevin/app/tracker/repository/mongo/IssueMongoRepository.java
+++ com.shemilikevin.app.tracker/src/main/java/com/shemilikevin/app/tracker/repository/mongo/IssueMongoRepository.java
@@ -1,5 +1,14 @@
 package com.shemilikevin.app.tracker.repository.mongo;
 
+import java.util.List;
+
+import com.shemilikevin.app.tracker.model.Issue;
+
 public class IssueMongoRepository {
+
+	public List<Issue> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
