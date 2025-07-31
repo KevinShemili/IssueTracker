5cef615698e87b1e368691c8e2a19e04ee707b0b
KevinShemili <kevinshemili5@gmail.com>
2025-07-31 15:49:47 +0200

    Implement findAll to make test compile

--- com.shemilikevin.app.tracker/src/test/java/com/shemilikevin/app/tracker/repository/mongo/IssueMongoRepositoryTest.java
+++ com.shemilikevin.app.tracker/src/test/java/com/shemilikevin/app/tracker/repository/mongo/IssueMongoRepositoryTest.java
@@ -1,16 +1,74 @@
 package com.shemilikevin.app.tracker.repository.mongo;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 import java.net.InetSocketAddress;
+import java.util.List;
+
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.shemilikevin.app.tracker.model.Issue;
 
 import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
 public class IssueMongoRepositoryTest {
 
+	private static final String DATABASE_NAME = "db";
+	private static final String COLLECTION_NAME = "collection";
 	private static MongoServer mongoServer;
 	private static InetSocketAddress inetSocketAddress;
 
 	private MongoClient mongoClient;
 	private IssueMongoRepository issueRepository;
+	private MongoCollection<Issue> issueCollection;
+
+	@BeforeClass
+	public static void setUpInMemoryServer() {
+		mongoServer = new MongoServer(new MemoryBackend());
+		inetSocketAddress = mongoServer.bind();
+	}
+
+	@AfterClass
+	public static void shutDownInMemoryServer() {
+		mongoServer.shutdown();
+	}
+
+	@Before
+	public void setUp() {
+		CodecRegistry codecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+				fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+		MongoClientOptions mongoClientOptions = MongoClientOptions.builder().codecRegistry(codecRegistry).build();
+
+		mongoClient = new MongoClient(new ServerAddress(inetSocketAddress), mongoClientOptions);
+		issueRepository = new IssueMongoRepository();
+		MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+		database.drop();
+		issueCollection = database.getCollection(COLLECTION_NAME, Issue.class);
+	}
+
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
+
+	@Test
+	public void testFindAll_EmptyDatabase_Returnsempty() {
+
+		// Act
+		List<Issue> issueList = issueRepository.findAll();
+	}
 }
