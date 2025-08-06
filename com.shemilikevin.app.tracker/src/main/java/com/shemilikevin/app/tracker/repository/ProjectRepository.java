3ad385fdd85d79fcccafe29dfbfba061ba20f115
KevinShemili <kevinshemili5@gmail.com>
2025-08-06 16:24:03 +0200

    Last unit tests

--- com.shemilikevin.app.tracker/src/main/java/com/shemilikevin/app/tracker/repository/ProjectRepository.java
+++ com.shemilikevin.app.tracker/src/main/java/com/shemilikevin/app/tracker/repository/ProjectRepository.java
@@ -6,9 +6,11 @@ import com.shemilikevin.app.tracker.model.Project;
 
 public interface ProjectRepository {
 
-	boolean exists(String projectId);
+	boolean exists(String id);
 
 	List<Project> findAll();
+
+	Project findById(String id);
 
 	void save(Project project);
 
